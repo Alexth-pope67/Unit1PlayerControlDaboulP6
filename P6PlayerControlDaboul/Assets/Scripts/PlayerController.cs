9c6721d8be860d29f767a1e58cffe8fc74249645
Alexth-pope67 <736521@student.dusd.net>
2026-02-10 13:49:29 -0800

    finished/starting
    finished the care and now starting to fix bugs for plane

--- P6PlayerControlDaboul/Assets/Scripts/PlayerController.cs
+++ P6PlayerControlDaboul/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
+    private float speed = 10.0f;
     private float turnSpeed = 100.0f;
     private float horizontalInput;
     private float forwardInput;
@@ -24,6 +24,16 @@ public class PlayerController : MonoBehaviour
         // Move The Vehicle Forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        // Axis setup
+        horizontalInput = Input.GetAxis("Horizontal"); 
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Rotate the vehicle left and right
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
         if (Input.GetKeyDown(switchKey))
