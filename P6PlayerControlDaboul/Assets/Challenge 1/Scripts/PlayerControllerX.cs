19a65a6395ab90f15c111cf5c0bb3c753d9ffadb
Alexth-pope67 <736521@student.dusd.net>
2026-02-23 13:52:03 -0800

    fixed challenge
    fixed scripts and able to do the obstacles

--- P6PlayerControlDaboul/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ P6PlayerControlDaboul/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +7,13 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float moveSpeed = 20f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -21,9 +23,22 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            // Move the object down on the Y-axis
+            transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        { 
+            // Move the object up on the Y-axis
+            transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
+        }
+
+
     }
 }
